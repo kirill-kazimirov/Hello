3170b9d9147040363b504ae879925ec43efc7801
kirilllka1993 <kirilllka1993@gmail.com>
2023-01-08 21:56:15 +0300

    Удалить комит

--- src/by/newhardskills/NeedToDeleteCommit.java
+++ src/by/newhardskills/NeedToDeleteCommit.java
@@ -2,6 +2,6 @@ package by.newhardskills;
 
 public class NeedToDeleteCommit {
     public static void main(String[] args) {
-        System.out.println("delete commit");
+        System.out.println("delete commitееееееее");
     }
 }
